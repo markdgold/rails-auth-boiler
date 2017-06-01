5db544b862a466cec728236411b8faaa06eda7ec
markdgold <mgoldstein@utexas.edu>
2017-06-01 12:08:42 -0700

    included auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  # get 'session/new'
+  get 'signup' => "signup#new"
+  post 'signup' => "signup#create"
+
+  get 'login' => "session#new"
+  post 'login' => "session#create"
+  delete 'login' => "session#destroy"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'main#index'
 
